96975d8c665aa7e955d75dedd79a57d21dad7ce3
reynardkoh <reynardkoh@gmail.com>
2024-04-25 11:31:20 +0800

    Calculating BMI

---
+++ bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi(height,weight):
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
+
+    BMI = weight / (height*height)
+    print("BMI =" , BMI)
+
+    return BMI
+
+bmi=calculate_bmi(weight= 57, height= 1.73)
+
+def weight_class(bmi):
+    if bmi<18.5 :
+        print("Under Weight")
+    elif bmi>=18.5 and bmi<=25.0:
+        print("Normal Weight")
+    else:
+       print("Over Weight")    
+
+weight_class(bmi)
