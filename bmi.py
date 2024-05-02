089e538dd9dc653b7579868ac86f92f2a26cc5c1
reynardkoh <reynardkoh@gmail.com>
2024-05-02 10:48:41 +0800

    Adding Input Options & Return Values

--- bmi.py
+++ bmi.py
@@ -7,15 +7,20 @@ def calculate_bmi(height,weight):
 
     return BMI
 
-bmi=calculate_bmi(weight= 57, height= 1.73)
+weightinp = float(input("What is your weight?"))
+heightinp = float(input("What is your height?"))
+bmi=calculate_bmi(heightinp,weightinp)
 
 def weight_class(bmi):
     if bmi<18.5 :
         print("Under Weight")
+        return -1
     elif bmi>=18.5 and bmi<=25.0:
         print("Normal Weight")
+        return 0
     else:
-       print("Over Weight")    
+       print("Over Weight")  
+       return 1  
 
 weight_class(bmi)
 
